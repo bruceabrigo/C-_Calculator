9249f3648091a8167e239614f6dc118e48cce004
Bruce Abrigo <bruceabrigo@outlook.com>
2024-03-15 13:06:25 -0500

    Addition method functioning properly

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,8 +18,50 @@ namespace Calculator
             double num1 = Convert.ToDouble(Console.ReadLine());
 
             // Prompt user to enter operand
+            Console.WriteLine("Please enter operand: +, -, *, /");
+            // get operand
+            char operand = Convert.ToChar(Console.ReadLine());
 
+            // Prompt user to enter second number
+            Console.WriteLine("Enter the second number.");
+            // get second number input
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // Create default result to 0
+            double result = 0;
+
+            // create switch case
+
+            switch (operand) {
+                case '+':
+                    // call function and pass in num1 and num2 params
+                    result = Add(num1, num2);
+                    break;
+                case '-':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+                // default will return false operand
+                default:
+                    Console.WriteLine("Operand incorrect");
+                    break;
+
+            }
+
+            Console.WriteLine("Result: " + result);
+            
+            // get user input to close app
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
 
         }
+        // define methods for each operand
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
     }
 }

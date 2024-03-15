b8d19ea1c2913aea3fd01e50516d0946183efa30
Bruce Abrigo <bruceabrigo@outlook.com>
2024-03-15 02:18:30 -0500

    Init commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Calcuator!");
+
+            // Prompt user to inter first number
+            Console.WriteLine("Enter the first nunmber.");
+            // get first number input
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Prompt user to enter operand
+
+
+        }
+    }
+}
